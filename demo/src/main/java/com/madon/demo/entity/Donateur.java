2ebcd3c7fcf9ec0c511347d2e57d4a3583bfb43e
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-01 16:18:08 +0100

    create donateur entity

--- demo/src/main/java/com/madon/demo/entity/Donateur.java
+++ demo/src/main/java/com/madon/demo/entity/Donateur.java
@@ -1,4 +1,28 @@
 package com.madon.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Donateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String telephone;
+    private String email;
+    private String username;
 }
