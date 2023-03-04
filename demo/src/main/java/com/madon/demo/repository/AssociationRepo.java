074220b8e5edbbcb05bc20e3c6593af9a7c24200
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-04 21:25:16 +0100

    create associationRepo interface

---
+++ demo/src/main/java/com/madon/demo/repository/AssociationRepo.java
@@ -0,0 +1,8 @@
+package com.madon.demo.repository;
+import com.madon.demo.entity.Association;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssociationRepo extends JpaRepository<Association, Long>{
+}
