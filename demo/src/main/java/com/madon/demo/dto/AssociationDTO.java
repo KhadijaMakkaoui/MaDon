3c2e1d0fdb6b4a7957f13743482d6731bdaedd09
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-04 21:43:19 +0100

    change lombok annotaion data by annotations of getters and setters

--- demo/src/main/java/com/madon/demo/dto/AssociationDTO.java
+++ demo/src/main/java/com/madon/demo/dto/AssociationDTO.java
@@ -1,9 +1,12 @@
 package com.madon.demo.dto;
 
 import com.madon.demo.entity.Association;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssociationDTO {
     private Long id;
     private String ref;
@@ -11,17 +14,6 @@ public class AssociationDTO {
     private String adresse;
     private String description;
     private String imagePath;
-
-    public AssociationDTO() {}
-
-    public AssociationDTO(Long id, String ref, String nom, String adresse, String description, String imagePath) {
-        this.id = id;
-        this.ref = ref;
-        this.nom = nom;
-        this.adresse = adresse;
-        this.description = description;
-        this.imagePath = imagePath;
-    }
 
     public static AssociationDTO convertToDTO(Association association) {
         return new AssociationDTO(
--- demo/src/main/java/com/madon/demo/service/DonateurService.java
+++ demo/src/main/java/com/madon/demo/service/DonateurService.java
@@ -1,4 +1,30 @@
 package com.madon.demo.service;
 
-public class DonateurService {
+import com.madon.demo.entity.Donateur;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DonateurDto {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String telephone;
+    private String email;
+    private String username;
+
+    public static DonateurDto fromEntity(Donateur donateur) {
+        return new DonateurDto(
+                donateur.getId(),
+                donateur.getNom(),
+                donateur.getPrenom(),
+                donateur.getAdresse(),
+                donateur.getTelephone(),
+                donateur.getEmail(),
+                donateur.getUsername()
+        );
+    }
 }
