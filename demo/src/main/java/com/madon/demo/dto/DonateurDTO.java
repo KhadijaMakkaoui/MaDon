b04f6124eb640434a2f2faebc2dd53760d85a377
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-04 21:44:40 +0100

    create donater dto

--- demo/src/main/java/com/madon/demo/dto/DonateurDTO.java
+++ demo/src/main/java/com/madon/demo/dto/DonateurDTO.java
@@ -1,4 +1,30 @@
 package com.madon.demo.dto;
 
-public class DonateurDto {
+import com.madon.demo.entity.Donateur;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DonateurDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String telephone;
+    private String email;
+    private String username;
+
+    public static DonateurDTO fromEntity(Donateur donateur) {
+        return new DonateurDTO(
+                donateur.getId(),
+                donateur.getNom(),
+                donateur.getPrenom(),
+                donateur.getAdresse(),
+                donateur.getTelephone(),
+                donateur.getEmail(),
+                donateur.getUsername()
+        );
+    }
 }
