eec307fd90d0014ebef8df265714be7f3c3198b2
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-28 09:48:40 +0000

    Make getAll method return DTO

--- demo/src/main/java/com/madon/demo/service/CompagneService.java
+++ demo/src/main/java/com/madon/demo/service/CompagneService.java
@@ -1,14 +1,22 @@
 package com.madon.demo.service;
 
+import com.madon.demo.dto.CompagneFondsDTO;
 import com.madon.demo.entity.CompagneFonds;
 import com.madon.demo.repository.CompagneFondsRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class CompagneService {
     private CompagneFondsRepo compagneFondsRepo;
+    private CompagneFondsDTO compagneFondsDTO;
+    public List<CompagneFondsDTO> getAllCompagne(){
+        return compagneFondsRepo.findAll().stream().map((compagneFonds -> compagneFondsDTO.convertToDTO(compagneFonds))).collect(Collectors.toList());
+    }
     public CompagneFonds createCompagne(CompagneFonds compagneFonds){
         return compagneFondsRepo.save(compagneFonds);
     }
