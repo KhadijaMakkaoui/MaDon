f1eb03fe361d66c5018b16820480f92b69ca6310
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-28 09:31:33 +0000

    create donnateur controller

--- demo/src/main/java/com/madon/demo/controller/DonateurController.java
+++ demo/src/main/java/com/madon/demo/controller/DonateurController.java
@@ -1,4 +1,9 @@
 package com.madon.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/donateur")
 public class DonateurController {
 }
