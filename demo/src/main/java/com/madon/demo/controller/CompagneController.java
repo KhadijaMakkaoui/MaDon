ecc4e126cef9300cc7d0a2f90a01805695502e88
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-29 10:31:24 +0000

    resolve error

--- demo/src/main/java/com/madon/demo/controller/CompagneController.java
+++ demo/src/main/java/com/madon/demo/controller/CompagneController.java
@@ -1,36 +1,36 @@
 package com.madon.demo.controller;
 
+import com.madon.demo.dto.CompagneFondsDTO;
 import com.madon.demo.entity.CompagneFonds;
 import com.madon.demo.service.CompagneService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/compagne")
+@RequestMapping("api/compagne")
 @AllArgsConstructor
 public class CompagneController {
     CompagneService compagneService;
     @GetMapping
-    public void getCompagne(){
-        compagneService.getAllCompagne();
+    public List<CompagneFondsDTO> getCompagne(){
+        return compagneService.getAllCompagne();
     }
     @GetMapping("/id")
     public CompagneFonds getCompagneById(Long id){
        return compagneService.getCompagne(id);
     }
     @PostMapping
-    public CompagneFonds createCompagne(CompagneFonds compagneFonds){
+    public CompagneFonds createCompagne(@RequestBody CompagneFonds compagneFonds){
         return compagneService.createCompagne(compagneFonds);
     }
     @PostMapping("/update")
-    public CompagneFonds updateCompagne(CompagneFonds compagneFonds){
+    public CompagneFonds updateCompagne(@RequestBody CompagneFonds compagneFonds){
         return compagneService.updateCompagne(compagneFonds);
     }
     @PostMapping("/id")
-    public void deleteCompagne(Long id){
+    public void deleteCompagne(@PathVariable Long id){
         compagneService.deleteCompagne(id);
     }
 }
